d53c708bb549b6be9e619f570b8f751c5bcbc1b0
mSparks43 <mark.parker01@googlemail.com>
2021-12-09 05:01:04 +0100

    cleanup settings window

--- src/SettingsWidget.cpp
+++ src/SettingsWidget.cpp
@@ -31,6 +31,7 @@ static XPWidgetID resetButton;
 static XPWidgetID statusCaption[5];
 static XPWidgetID w_top;
 static XPWidgetID w_bottom;
+static XPWidgetID ipCaption;
 static bool hasSettingsWindow;
 SettingsWidget::SettingsWidget(){
 
@@ -51,6 +52,8 @@ void SettingsWidget::setIP(char * jvmBuffer,char *ipbuffer){
      sprintf(com,"Network mobile:phone=%s",ipbuffer);
       JVM* jvmO;
       jvmO=getJVM();
+      if(!jvmO->hasjvm)
+        return;
      jvmO->getData(com);
      XPLMUnregisterFlightLoopCallback(displaySettingsStatus, NULL);
 	XPDestroyWidget(w_window, 1);
@@ -67,48 +70,13 @@ void SettingsWidget::setSlave(int isSlave){
      sprintf(com,"Network mobile:isSlave=%d",isSlave);
       JVM* jvmO;
       jvmO=getJVM();
+      if(!jvmO->hasjvm)
+        return;
      jvmO->getData(com);
      jvmO->setisSlave((long)isSlave);
 }
 
-/*void SettingsWidget::test(char * jvmBuffer,char *ipbuffer){
 
-     JVM* jvmO;
-     try{
-         
-         jvmO=getJVM();
-         bool lockedIn=!jvmO->loadedLibrary;
-         //if(jvmO->hasjvm){
-        jvmO->deactivateJVM();
-         //}  
-         //else{
-         //initJVM();
-         //jvmO=getJVM();
-        jvmO->init_parameters(jvmBuffer);//"/home/mSparks/Downloads/jre1.8.0_181/lib/amd64/server/libjvm.so");
-        jvmO->activateJVM();
-
-         //}
-        // jvmO->hasjvm=false;
-         char com[512];
-         if(jvmO->hasjvm&&lockedIn){
-             #if defined(__linux__)
-                sprintf(com,"Network path:lin=%s",jvmBuffer);
-            #elif defined(_WIN64)
-                sprintf(com,"Network path:win=%s",jvmBuffer);
-            #elif defined(__APPLE__)
-                sprintf(com,"Network path:mac=%s",jvmBuffer);
-            #endif
-             
-            jvmO->getData(com);
-         }
-          jvmO->start();
-            sprintf(com,"Network mobile:phone=%s",ipbuffer);
-            jvmO->getData(com);
-     }catch(...){
-		printf("Exception during JVM test\n");
-		return;
-	}   
-}*/
 
 
 void SettingsWidget::reset(){
@@ -192,7 +160,7 @@ void SettingsWidget::init(){
 					0,		// root
 					w_window,
 					xpWidgetClass_SubWindow);  
-      XPSetWidgetProperty(w_top, xpProperty_SubWindowType, xpSubWindowStyle_SubWindow);                            
+      XPSetWidgetProperty(w_top, xpProperty_SubWindowType, xpSubWindowStyle_Screen);                            
    // XPSetWidgetProperty(w_top, xpProperty_SubWindowType, xpMainWindowStyle_Translucent);
     w_bottom = XPCreateWidget(x+50, y-102, x2-10, y2+10,
 					1,	// Visible
@@ -200,34 +168,18 @@ void SettingsWidget::init(){
 					0,		// root
 					w_window,
 					xpWidgetClass_SubWindow);  
-      XPSetWidgetProperty(w_bottom, xpProperty_SubWindowType, xpSubWindowStyle_SubWindow);                            
+      XPSetWidgetProperty(w_bottom, xpProperty_SubWindowType, xpSubWindowStyle_Screen);                            
   //  XPSetWidgetProperty(w_bottom, xpProperty_SubWindowType, xpMainWindowStyle_Translucent);
        XPAddWidgetCallback(w_window, SettingsWidgetsHandler); 
     JVM* jvmO=getJVM();
-    /*XPCreateWidget(x+10, y-20, x+60, y-40,
-					1, "JVM:", 0, w_window,
-					xpWidgetClass_Caption);
-//XPSetWidgetProperty(jvmCaption, xpProperty_CaptionLit, 1);
- 
- #if defined(__linux__)
-  char *lib=jvmO->lin;
-   #elif defined(_WIN64)
-   char *lib=jvmO->win;
-   #elif defined(__APPLE__)
-   char *lib=jvmO->mac;
-   #endif
-    jvmField = XPCreateWidget(x+70, y-20, x2-20, y-40,
-					1, lib, 0, w_window,
-					xpWidgetClass_TextField);
-    XPSetWidgetProperty(jvmField, xpProperty_TextFieldType, xpTextEntryField);
-	XPSetWidgetProperty(jvmField, xpProperty_Enabled, !jvmO->loadedLibrary);*/
 
-    XPCreateWidget(x+10, y-50, x+60, y-60,
+
+    ipCaption=XPCreateWidget(x+10, y-50, x+60, y-60,
 					1, "Device IP:", 0, w_window,
 					xpWidgetClass_Caption);
-    //XPSetWidgetProperty(ipCaption, xpProperty_CaptionLit, 1);
+    XPSetWidgetProperty(ipCaption, xpProperty_CaptionLit, 1);
     char* device=jvmO->getDevice();
-    ipField = XPCreateWidget(x+70, y-50, x2-20, y-70,
+    ipField = XPCreateWidget(x+70, y-45, x2-20, y-65,
 					1, device, 0, w_window,
 					xpWidgetClass_TextField);
     XPSetWidgetProperty(ipField, xpProperty_TextFieldType, xpTextEntryField);
@@ -238,31 +190,23 @@ void SettingsWidget::init(){
     XPSetWidgetProperty(isSlaveField, xpProperty_ButtonBehavior, xpButtonBehaviorCheckBox);
     long isSlave=jvmO->getisSlave();
     XPSetWidgetProperty(isSlaveField, xpProperty_ButtonState, isSlave);
-    XPCreateWidget(x+51, y-70, x+160, y-100,
+     XPWidgetID ecCaption =XPCreateWidget(x+51, y-70, x+160, y-100,
 					1, "Extended Cockpit", 0, w_window,
 					xpWidgetClass_Caption);
-
-   /* testButton = XPCreateWidget(x2-140, y-70, x2-80, y-90,
-					1, "Connect", 0, w_window,
-					xpWidgetClass_Button);*/
-
+    XPSetWidgetProperty(ecCaption, xpProperty_CaptionLit, 1);    
     XPSetWidgetProperty(resetButton, xpProperty_ButtonType, xpPushButton);
-
-    /*testButton = XPCreateWidget(x2-110, y-70, x2-70, y-90,
-					1, "Test", 0, w_window,
-					xpWidgetClass_Button);
-
-    XPSetWidgetProperty(testButton, xpProperty_ButtonType, xpPushButton);*/
 
     setButton = XPCreateWidget(x2-60, y-70, x2-20, y-90,
 					1, "Apply", 0, w_window,
 					xpWidgetClass_Button);
 
     XPSetWidgetProperty(setButton, xpProperty_ButtonType, xpPushButton);
-    for(int i=0;i<5;i++)
-    statusCaption[i]=XPCreateWidget(x+60, y-112-i*15, x2-20, y-122-i*15,
+    for(int i=0;i<5;i++){
+        statusCaption[i]=XPCreateWidget(x+60, y-112-i*15, x2-20, y-122-i*15,
 					1, " ", 0, w_window,
 					xpWidgetClass_Caption);
+         XPSetWidgetProperty(statusCaption[i], xpProperty_CaptionLit, 1);            
+    }
 
                     //xpWidgetClass_TextField);
    // XPSetWidgetProperty(statusCaption, xpProperty_TextFieldType, xpTextEntryField);
